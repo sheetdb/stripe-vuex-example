dc2c30dd2f750f4e73f34030390a038648e0313a
Krzysztof Świtalski <kubuss88@gmail.com>
2019-04-12 01:52:53 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="container">
+    <div>
+
+      <form action="/charge" method="POST">
+        <script
+          src="https://checkout.stripe.com/checkout.js"
+          class="stripe-button"
+          data-key="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
+          data-image="/square-image.png"
+          data-name="Demo Site"
+          data-description="2 widgets ($20.00)"
+          data-amount="2000">
+        </script>
+      </form>
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ server/index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+var bodyParser = require('body-parser')
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const { host, port } = nuxt.options.server
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use( bodyParser.json() )
+
+  app.post('/charge', function (req, res) {
+    let token = req.body.stripeToken;
+    let email = req.body.stripeEmail;
+    const stripe = require('stripe')('PASTE_HERE_YOUR_PRIVATE_KEY');
+
+    // const customer = await stripe.customers.create({
+    //   email: req.body.stripeToken
+    // });
+
+    stripe.customers.create({
+      email: email
+    }).then((customer) => {
+      return stripe.customers.createSource(customer.id, {
+        source: 'tok_visa'
+      });
+    }).then((source) => {
+      return stripe.charges.create({
+        amount: 1600, // amount in cents; 1600 = 16,00 USD
+        currency: 'usd',
+        customer: source.customer
+      });
+    }).then((charge) => {
+      // here enter logic after card was successfully charged
+      res.redirect('/charge')
+    }).catch((err) => {
+      // here enter logic after card was unsuccessfully charged
+      res.redirect('/error')
+    });
+  })
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
